0695fb7bd9cdd55db48dd7a2e5f7d5b32d39a046
Calebe Rocha <angelo.calebe@acad.pucrs.br>
2020-05-25 01:25:05 -0300

    AvaliaçãoController renomeado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,7 @@ const ProfessorController = require('./controllers/ProfessorController');
 const GrupoProvisorioController = require('./controllers/GrupoProvisorioController');
 const AlunoSessionController = require('./controllers/AlunoSessionController');
 const GrupoFinalController = require('./controllers/GrupoFinalController');
-const AvaliaçãoController = require('./controllers/AvaliaçãoController');
+const AvaliacaoController = require('./controllers/AvaliacaoController');
 const ProfessorSessionController = require('./controllers/ProfessorSessionController');
  
 const routes = express.Router();
@@ -15,6 +15,7 @@ routes.post('/alunoSession', AlunoSessionController.login);
 routes.post('/professorSession', ProfessorSessionController.login);
 
 routes.get('/alunos', AlunoController.index);
+routes.get('/alunos/:id', AlunoController.get);
 routes.post('/alunos', AlunoController.create);
 
 routes.get('/professores', ProfessorController.index);
@@ -23,12 +24,12 @@ routes.post('/professores', ProfessorController.create);
 routes.get('/gruposProvisorios', GrupoProvisorioController.index);
 routes.post('/gruposProvisorios', GrupoProvisorioController.create);
 routes.delete('/gruposProvisorios/:id', GrupoProvisorioController.delete);
-routes.put('/gruposProvisorios/:id', GrupoProvisorioController.alterar);
+routes.put('/gruposProvisorios/:id', GrupoProvisorioController.update);
 
 routes.get('/gruposFinais', GrupoFinalController.index);
 routes.post('/gruposFinais', GrupoFinalController.create);
 
-routes.get('/avaliacoes', AvaliaçãoController.index);
-routes.post('/avaliacoes', AvaliaçãoController.create);
+routes.get('/avaliacoes', AvaliacaoController.index);
+routes.post('/avaliacoes', AvaliacaoController.create);
  
 module.exports = routes; 
