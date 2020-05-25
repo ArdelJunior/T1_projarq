a53d75eea019f7cfdd9babb20e54cb6abc6d6f60
Calebe Rocha <angelo.calebe@acad.pucrs.br>
2020-05-24 22:12:16 -0300

    Validações de aluno

--- backend/src/controllers/AlunoController.js
+++ backend/src/controllers/AlunoController.js
@@ -1,26 +1,61 @@
-const crypto= require ('crypto'); 
+const { v4: uuid} = require('uuid');
 const connection = require('../database/connection'); 
  
 module.exports = {
  
     async index (request, response) {
-        const alunos = await connection ('alunos').select('*');
+        const alunos = await connection('alunos as a')
+            .select(['a.id', 'a.matricula', 'a.nome', 'c.nome as curso'])
+            .join('cursos as c', 'a.curso', 'c.id');
     
         return response.json(alunos);
     }, 
  
     async create (request, response) {
     
-        const {matricula, nome, curso} = request.body; 
+        const {matricula, nome, curso} = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        if(!matricula) {
+            return response.status(400).json({error: "Matrícula não inserida"});
+        }
+        if(!nome) {
+            return response.status(400).json({error: "Nome não inserido"});
+        }
+        if(!curso) {
+            return response.status(400).json({error: "Curso não inserido"});
+        }
+
+        const id = uuid();
+
+        const cursos = await connection('cursos')
+            .select('id').where('nome', '=', curso);
+
+        let idCurso;
+        
+        try {
+            idCurso = cursos[0]['id'];
+            if(idCurso === undefined) {
+                throw new Error('');
+            }
+        }
+        catch(e) {
+            return response.status(400).json({error: `Curso ${curso} não encontrado`});
+        }
             
-        await connection ('alunos').insert({ 
-            matricula,
-            id,
-            nome,
-            curso,
-        })
-        return response.status(201).json({id});
+        try {
+            await connection ('alunos').insert({
+                id,
+                matricula,
+                nome,
+                curso: idCurso,
+            })
+            return response.status(201).json({id});
+        } catch(e) {
+            console.error(e);
+            // if(/constraint/i.test(e.code)) {
+            //     return response.status(400).json({error: `O aluno ${matricula} já existe`});
+            // }
+            return response.status(400).json({e});
+        }
     }
 };
