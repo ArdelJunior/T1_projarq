610877a1a597420cf7f15f7229a64d2b09ed53e5
Calebe Rocha <angelo.calebe@acad.pucrs.br>
2020-07-01 15:18:45 -0300

    Correção na relação alunos_times_sugeridos

--- backend/src/database/migrations/20200527000007_create_alunos_times_sugeridos.js
+++ backend/src/database/migrations/20200527000007_create_alunos_times_sugeridos.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable("alunos_times_sugeridos", function(table) {
     table.increments();
     table.integer("id_aluno").notNullable().references("id").inTable("alunos");
-    table.integer("id_time").notNullable().references("id").inTable("times");
+    table.integer("id_time_sugerido").notNullable().references("id").inTable("times_sugeridos");
   });
 };
 
