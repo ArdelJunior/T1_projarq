f81d9ba67e45df0a70bdb4c5462fe854e7538cb1
Calebe Rocha <angelo.calebe@acad.pucrs.br>
2020-07-06 14:00:48 -0300

    Node servindo arquivos em build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes'); 
 const app = express();
 
@@ -8,3 +9,8 @@ app.listen(3333);
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
